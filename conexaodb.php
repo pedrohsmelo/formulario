5fe4dd97885f282223f895f33a4911a9cf46d1c7
Pedro Henrique Melo <101877681+pedrohsmelo@users.noreply.github.com>
2023-07-11 18:11:45 -0300

    Add files via upload

--- conexaodb.php
+++ conexaodb.php
@@ -2,18 +2,20 @@
 session_start();
 
 $servername = "localhost";
-$username = "nomedeusuario";
-$password = "senhadoseubanco";
-$dbname = "nomedobanco";
+$username = "admin";
+$password = "123qwe!@#QWE";
+$dbname = "banco_formulario";
 
 $conexao = new mysqli($servername, $username, $password, $dbname);
 
 // verificar a conexão
 if ($conexao->connect_error) {
-    die("Falha na conexão " . $conexao->connect_error);
+    die("Falha na conexão " . $conn->connect_error);
 }
 
 require_once 'crud.php';
+
+
 
 if ($conexao->query($sql) === TRUE) {
     $_SESSION['mensagem'] = 'Dados salvos com sucesso!';
@@ -23,6 +25,6 @@ if ($conexao->query($sql) === TRUE) {
 
 $conexao->close();
 
-header("Location: formulario.html?mensagem=" . urlencode($_SESSION['mensagem']));
+header("Location: index.html?mensagem=" . urlencode($_SESSION['mensagem']));
 exit();
 ?>
--- salvardados.php
+++ salvardados.php
@@ -3,7 +3,10 @@
         $sobrenome = $_POST['sobrenome'];
         $idade = $_POST['idade'];
         $email = $_POST['email'];
+        $celular = $_POST['celular'];
         $estado = $_POST['estado'];
+        $pais = $_POST['pais'];
+        $whats = isset($_POST['whats']) ? 1 : 0;
         
         require_once 'conexaodb.php';
 ?>
